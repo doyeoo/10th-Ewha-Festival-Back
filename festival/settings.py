4664fc1fc9996fb9ca95d22c67de61fc7a0d47d3
doyeoo <ehdusla013@gmail.com>
2022-08-08 17:46:02 +0900

    [chore] settings.py 수정

--- festival/settings.py
+++ festival/settings.py
@@ -44,12 +44,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
     'account',
     'board',
     'notice',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
