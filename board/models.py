94a08d0eb97b0076ec2bea519d3550dfeead0c02
doyeoo <ehdusla013@gmail.com>
2022-08-10 23:39:14 +0900

    [feat] 부스 좋아요 필드 추가

--- board/models.py
+++ board/models.py
@@ -31,12 +31,13 @@ class Day(models.Model):
 
 class Booth(TimeStamp):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    day = models.ManyToManyField(Day, related_name='post')
-    college = models.CharField(max_length=20, default='경영관')
+    day = models.ManyToManyField(Day, related_name='booths')
+    college = models.CharField(max_length=20, blank=True)
     name = models.TextField()
     image = models.URLField(blank=True)
     notice = models.TextField(blank=True)
     description = models.TextField(blank=True)
+    like = models.ManyToManyField(User, related_name='booths', blank=True)
 
     def __str__(self):
         return self.name
@@ -54,3 +55,4 @@ class Comment(TimeStamp):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Booth, on_delete=models.CASCADE)
     content = models.TextField()
+
