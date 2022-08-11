719254bcfce6b4c7c58bf3135026d273c40059da
doyeoo <ehdusla013@gmail.com>
2022-08-11 16:24:34 +0900

    [feat] 부스 목록 필터링

--- board/views.py
+++ board/views.py
@@ -11,7 +11,19 @@ class BoothListView(views.APIView):
     serializer_class = BoothSerializer
 
     def get(self, request):
-        booths = Booth.objects.all()
+        user = request.user
+        
+        day = request.GET.get('day')
+        college = request.GET.get('college')
+
+        params = {'day': day, 'college': college}
+        arguments = {}
+        for key, value in params.items():
+            if value:
+                arguments[key] = value
+
+        booths = Booth.objects.filter(**arguments)
+
         serializer = self.serializer_class(booths, many=True)
 
         return Response({'message': '부스 목록 조회 성공', 'data': serializer.data}, status=HTTP_200_OK)
@@ -21,7 +33,12 @@ class BoothDetailView(views.APIView):
     serializer_class = BoothSerializer
 
     def get(self, request, pk):
+        user = request.user
         booth = get_object_or_404(Booth, pk=pk)
+
+        if booth.like.filter(pk=user.id).exists():
+            booth.is_liked=True
+
         serializer = self.serializer_class(booth)
 
         return Response({'message': '부스 상세 조회 성공', 'data': serializer.data}, status=HTTP_200_OK)
