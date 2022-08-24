ae8d815b1d345ffb62e8b5ce773ecc188db8db89
yumiiiiiii <102947194+yumiiiiiii@users.noreply.github.com>
2022-08-25 01:56:27 +0900

    [feat] TF 공지 작성

--- notice/views.py
+++ notice/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import views
+from rest_framework.status import *
+from .models import *
+from .serializers import *
 
 # Create your views here.
+
+class NoticeView(views.APIView):
+    serializer_class = NoticeSerializer
+
+    def post(self, request):
+        serializer=NoticeSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'message': 'TF 공지 작성 성공', 'data': serializer.data}, status=HTTP_200_OK)
+        else:
+            return Response({'message': 'TF 공지 작성 실패', 'data': serializer.errors}, status=HTTP_400_BAD_REQUEST)
