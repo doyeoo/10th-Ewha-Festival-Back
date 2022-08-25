2ca23aeea1fe68963b13cc1b0a5b1bf9f0f8901c
doyeoo <ehdusla013@gmail.com>
2022-08-25 15:14:53 +0900

    [chore] appsec 파일

--- festival/settings.py
+++ festival/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = env('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 AUTH_USER_MODEL = 'account.User'
 
