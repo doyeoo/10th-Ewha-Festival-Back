1bda3a84b324d2fd8fcb94b4f28b1c7849cc2a06
doyeoo <81256252+doyeoo@users.noreply.github.com>
2022-08-26 20:34:18 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,6 +3,7 @@ cd /home/ubuntu/app
 python -m venv venv
 source venv/bin/activate
 
+sudo apt-get install python-pip
 pip install -r requirements.txt
 python manage.py collectstatic
 python manage.py migrate
