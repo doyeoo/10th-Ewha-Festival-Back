fba5255be9d74966e9412b270ffd0128e87d1cdc
doyeoo <81256252+doyeoo@users.noreply.github.com>
2022-08-26 20:32:56 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,11 +1,10 @@
 cd /home/ubuntu/app
 
-apt-get install python3-venv
-python3 -m venv venv
+python -m venv venv
 source venv/bin/activate
 
 pip install -r requirements.txt
-python3 manage.py collectstatic
-python3 manage.py migrate
+python manage.py collectstatic
+python manage.py migrate
 deactivate
 sudo systemctl restart uwsgi
