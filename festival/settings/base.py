405e3109a563c46058a0067d380098a1889e1212
doyeoo <ehdusla013@gmail.com>
2022-08-27 20:16:20 +0900

    [feat] CORS 세팅

--- festival/settings/base.py
+++ festival/settings/base.py
@@ -66,6 +66,14 @@ SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('Bearer',),
 }
 
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOWED_ORIGINS = [
+    'http://127.0.0.1:8000',
+    'http://localhost:8000',
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
+]
+
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
