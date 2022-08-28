47e0503bf8650b2eb21a0640e1d4e3f0f551a4d1
doyeoo <ehdusla013@gmail.com>
2022-08-28 21:07:39 +0900

    [fix] 부스 좋아요 응답 수수정

--- booth/views.py
+++ booth/views.py
@@ -110,7 +110,7 @@ class LikeView(views.APIView):
 
         if serializer.is_valid():
             serializer.save()
-            return Response({'message': '부스 좋아요 성공', 'data': serializer.data}, status=HTTP_200_OK)
+            return Response({'message': '부스 좋아요 성공', 'data': {'booth': serializer.data['id'], 'is_liked': serializer.data['is_liked']}}, status=HTTP_200_OK)
         else:
             return Response({'message': '부스 좋아요 실패', 'data': serializer.errors}, status=HTTP_400_BAD_REQUEST)
     
@@ -124,7 +124,7 @@ class LikeView(views.APIView):
 
         if serializer.is_valid():
             serializer.save()
-            return Response({'message': '부스 좋아요 취소 성공', 'data': serializer.data}, status=HTTP_200_OK)
+            return Response({'message': '부스 좋아요 취소 성공', 'data': {'booth': serializer.data['id'], 'is_liked': serializer.data['is_liked']}}, status=HTTP_200_OK)
         else:
             return Response({'message': '부스 좋아요 취소 실패', 'data': serializer.errors}, status=HTTP_400_BAD_REQUEST)
 
