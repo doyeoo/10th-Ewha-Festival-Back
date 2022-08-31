e3062b86bc0641c8eb84bf89850d2a453e8d88e2
doyeoo <ehdusla013@gmail.com>
2022-08-31 13:25:31 +0900

    [fix] 대표 사진 필드 수정

--- booth/models.py
+++ booth/models.py
@@ -46,7 +46,7 @@ class Booth(TimeStamp):
     college = models.CharField(choices=COLLEGE_CHOICES, max_length=20)
     name = models.TextField()
     number = models.CharField(max_length=10, blank=True)
-    thumnail = models.FileField(null=True, blank=True)
+    thumnail = models.URLField(null=True, blank=True)
     notice = models.TextField(blank=True)
     description = models.TextField(blank=True)
     like = models.ManyToManyField(User, related_name='booths', blank=True)
