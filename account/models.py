690bb51688a02e9f0806c39caf34b3bf1c62faa8
doyeoo <ehdusla013@gmail.com>
2022-09-06 01:41:32 +0900

    [fix] 닉네임 길이 수정

--- account/models.py
+++ account/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    nickname=models.CharField(max_length=20)
+    nickname=models.CharField(max_length=100)
     is_booth=models.BooleanField(default=True)
     is_tf=models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
