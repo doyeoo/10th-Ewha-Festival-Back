45644a16c7c61fe31702e20a44674776684f15e2
doyeoo <ehdusla013@gmail.com>
2022-09-07 01:00:27 +0900

    [fix] 유저 닉네임 반환

--- account/models.py
+++ account/models.py
@@ -7,3 +7,6 @@ class User(AbstractUser):
     is_tf=models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.username} ({self.nickname})'
