225cf29dee5956b61f1851164209bbe21e25b40e
doyeoo <ehdusla013@gmail.com>
2022-09-08 23:38:10 +0900

    [fix] 유저 기본 속성 변경

--- account/models.py
+++ account/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     nickname=models.CharField(max_length=100)
-    is_booth=models.BooleanField(default=True)
+    is_booth=models.BooleanField(default=False)
     is_tf=models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
